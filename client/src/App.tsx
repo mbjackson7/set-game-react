c0a588250b1661b9cb72fc5bef23e1b5bf62c942
Morgan Jackson <morgan.jackson@ocelotconsulting.com>
2023-08-12 18:07:05 -0500

    Remaining cards counter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,34 +6,31 @@ import { CardAttributes } from './models/card'
 function App() {
   const [onTable, setOnTable] = useState<CardAttributes[]>([])
   const [selected, setSelected] = useState<number[]>([])
-  const deck = useRef<CardAttributes[]>([])
+  const [deck, setDeck] = useState<CardAttributes[]>([])
   const colors = ['red', 'green', 'purple']
   const shapes = ['oval', 'diamond', 'squiggle']
   const numbers = [1, 2, 3]
   const shadings = ['fill', 'striped', 'outline']
   const rotate = window.innerWidth < window.innerHeight ? 'rotate-90' : ''
-  const [overflowLevel, setOverflowLevel] = useState<number>(1)
+  const [overflowLevel, setOverflowLevel] = useState<number>(0)
 
   // build a deck of cards
   useEffect(() => {
-    deck.current = []
+    let newDeck: CardAttributes[] = []
     colors.forEach(color => {
       shapes.forEach(shape => {
         numbers.forEach(number => {
           shadings.forEach(shading => {
-            deck.current.push({ shape: shape, color: color, number: number, shading: shading })
+            newDeck.push({ shape: shape, color: color, number: number, shading: shading })
           })
         })
       })
     });
     // shuffle the deck
-    deck.current.sort(() => Math.random() - 0.5)
-  }, [colors, shapes, numbers, shadings])
-
-  // deal 12 cards
-  useEffect(() => {
-    setOnTable(deck.current.slice(0, 15))
-  }, [deck])
+    newDeck.sort(() => Math.random() - 0.5)
+    setOnTable(newDeck.slice(0, 12))
+    setDeck(newDeck.slice(12))
+  }, [])
 
   const toggleSelected = (index: number) => {
     if (selected.includes(index)) {
@@ -58,17 +55,21 @@ function App() {
       const selectedCards = selected.map(i => onTable[i])
       if (isSet(selectedCards)) {
         let newTable = onTable
+        let newDeck = deck
         selected.forEach(i => {
           if (overflowLevel === 0) {
-            newTable[i] = deck.current.pop()!
+            newTable[i] = newDeck.pop()!
           } else {
             newTable.splice(i, 1)
           }
         })
         setTimeout(() => {
           setOnTable(newTable)
+          setDeck(newDeck)
           setSelected([])
-          setOverflowLevel(overflowLevel - 1)
+          if (overflowLevel !== 0) {
+            setOverflowLevel(overflowLevel - 1)
+          }
         }, 3000)
       } else {
         setTimeout(() => {
@@ -80,7 +81,7 @@ function App() {
 
   return (
     <div className='h-screen w-screen flex flex-col items-center justify-center p-10'>
-      <h1 className="text-4xl font-bold h-1/6">Set</h1>
+      <h1 className="text-4xl font-bold h-1/6">Remaining Cards: {deck.length + onTable.length}</h1>
       <div className={`grid grid-rows-3 grid-cols-${overflowLevel + 4} gap-2 h-5/6 max-h-[96vw] max-w-screen aspect-square ${rotate}`}>
         {
           onTable.map((card, index) => {
