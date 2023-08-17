f45beaec31824dbc66eab31b38f67c42785da9aa
Morgan Jackson <morgan.jackson@ocelotconsulting.com>
2023-08-16 20:56:30 -0500

    messages always in front

--- client/src/components/MessageModal.tsx
+++ client/src/components/MessageModal.tsx
@@ -1,7 +1,7 @@
 export default function MessageModal(props: any){
     const message = props.message;
     return(
-        <div className="absolute h-screen w-screen flex justify-center items-center">
+        <div className="absolute h-screen w-screen flex justify-center items-center z-50">
             <div className={`bg-${message.color}-800 text-3xl text-white border-2 border-black p-5 flex justify-center items-center`}>
                 {message.text}
             </div>
